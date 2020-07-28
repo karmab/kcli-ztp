34aa1d4c777b10ef78758966d531f85c041922ae
Yuval Kashtan <yuvalkashtan@gmail.com>
2020-07-28 10:56:50 +0300

    add oc bash-completion support

--- 02_packages.sh
+++ 02_packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-dnf -y install libvirt-libs libvirt-client ipmitool mkisofs tmux make git
+dnf -y install libvirt-libs libvirt-client ipmitool mkisofs tmux make git bash-completion
 {% if 'rhel' in image %} 
 subscription-manager repos --enable=openstack-15-tools-for-rhel-8-x86_64-rpms
 dnf -y install python3-openstackclient python3-ironicclient
