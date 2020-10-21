8570d60f0adf8918c91d31ae29cf149c1763e1c5
karmab <karimboumedhel@gmail.com>
2020-10-21 12:29:24 +0200

    fix typo

--- helpers/set_ironic_creds.sh
+++ helpers/set_ironic_creds.sh
@@ -3,5 +3,5 @@ export IRONIC_USER=$(oc get secret/metal3-ironic-password -o template --template
 export IRONIC_PASSWORD=$(oc get secret/metal3-ironic-password -o template --template '{{.data.password}}' | base64 -d)
 export INSPECTOR_USER=$(oc get secret/metal3-ironic-inspector-password -o template --template '{{.data.username}}' | base64 -d)
 export INSPECTOR_PASSWORD=$(oc get secret/metal3-ironic-inspector-password -o template --template '{{.data.password}}' | base64 -d)
-mv /root/clouds.yaml /root/clouds.yaml/ori
-envsubst < /root/clouds.yaml/ori > /root/clouds.yaml
+mv /root/clouds.yaml /root/clouds.yaml.ori
+envsubst < /root/clouds.yaml.ori > /root/clouds.yaml
