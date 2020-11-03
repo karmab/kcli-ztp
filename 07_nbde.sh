474b60b3559794081bf804518b6614eed2aa8e2f
karmab <karimboumedhel@gmail.com>
2020-11-03 21:15:04 +0100

    nbde fixes

--- 07_nbde.sh
+++ 07_nbde.sh
@@ -23,7 +23,7 @@ EOM
 ) | base64 -w0)
 export ROLE=worker
 envsubst < /root/99-openshift-tang-encryption-clevis.sample.yaml > /root/manifests/99-openshift-worker-tang-encryption-clevis.yaml
-envsubst < /root/99-openshift-tang-encryption-ka.yaml.sample > /root/manifests/99-openshift-worker-tang-encryption-ka.yaml
+envsubst < /root/99-openshift-tang-encryption-ka.sample.yaml > /root/manifests/99-openshift-worker-tang-encryption-ka.yaml
 export ROLE=master
 envsubst < /root/99-openshift-tang-encryption-clevis.sample.yaml > /root/manifests/99-openshift-master-tang-encryption-clevis.yaml
-envsubst < /root/99-openshift-tang-encryption-ka.yaml.sample > /root/manifests/99-openshift-master-tang-encryption-ka.yaml
+envsubst < /root/99-openshift-tang-encryption-ka.sample.yaml > /root/manifests/99-openshift-master-tang-encryption-ka.yaml
