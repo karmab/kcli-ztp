d6648ab512ae861284d68da14cf6edad30e49559
karmab <karimboumedhel@gmail.com>
2021-02-22 10:44:41 +0100

    dont deploy provisioning nics when no provisioning

--- 03_network.sh
+++ 03_network.sh
@@ -4,8 +4,10 @@ nmcli connection add ifname {{ baremetal_net }} type bridge con-name {{ baremeta
 nmcli con add type bridge-slave ifname eth0 master {{ baremetal_net }}
 nmcli con down "System eth0"; sudo pkill dhclient; sudo dhclient {{ baremetal_net }}
 
+{% if provisioning_enable %}
 nmcli connection add ifname {{ provisioning_net }} type bridge con-name {{ provisioning_net }}
 nmcli con add type bridge-slave ifname eth1 master {{ provisioning_net }}
 nmcli connection modify {{ provisioning_net }} ipv4.addresses {{ provisioning_installer_ip }}/{{ provisioning_cidr.split('/')[1] }} ipv4.method manual
 nmcli con down {{ provisioning_net }}
 nmcli con up {{ provisioning_net }}
+{% endif %}
