b97f04c7ba2efafe11c1b0f2cc8af9e50b24edc4
karmab <karimboumedhel@gmail.com>
2021-03-08 13:45:03 +0100

    fix bad indent

--- helpers/clean.sh
+++ helpers/clean.sh
@@ -8,8 +8,9 @@ if [ "$bootstrap" != "" ] ; then
 for vm in $bootstrap ; do
 virsh destroy $vm
 virsh undefine $vm
-virsh vol-delete $vm default
-virsh vol-delete $vm.ign default
+# virsh vol-delete $vm default
+# virsh vol-delete $vm.ign default
+for vol in $(virsh vol-list default | grep "$cluster.*bootstrap"  | awk '{print $2}') ; do echo $vol ; done
 virsh pool-info $vm >/dev/null 2>&1 && virsh pool-undefine $vm
 done
 fi
