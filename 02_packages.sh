6f8e8a9b78b6bba789baf901724988bdf75a6ae1
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2021-03-12 14:11:39 +0100

    Update 02_packages.sh

--- 02_packages.sh
+++ 02_packages.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+#fix the pyyaml version error
+pip3 install pip==8.1.1
+pip3 uninstall pyyaml
+pip3 install -U pip
 
 dnf -y install libvirt-libs libvirt-client ipmitool mkisofs tmux make git bash-completion
 dnf -y install python36
 export CRYPTOGRAPHY_DONT_BUILD_RUST=1
-pip3 install -U pip
 pip3 install python-ironicclient
