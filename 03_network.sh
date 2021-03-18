e592a36056abe26af43d2ef8b8ea72a385126f82
karmab <karimboumedhel@gmail.com>
2021-03-18 15:53:51 +0100

    run ifdown eth1 just in case

--- 03_network.sh
+++ 03_network.sh
@@ -8,6 +8,7 @@ nmcli con down "System eth0"; sudo pkill dhclient; sudo dhclient {{ baremetal_ne
 nmcli connection add ifname {{ provisioning_net }} type bridge con-name {{ provisioning_net }}
 nmcli con add type bridge-slave ifname eth1 master {{ provisioning_net }}
 nmcli connection modify {{ provisioning_net }} ipv4.addresses {{ provisioning_installer_ip }}/{{ provisioning_cidr.split('/')[1] }} ipv4.method manual
+nmcli con down "System eth1"
 nmcli con down {{ provisioning_net }}
 nmcli con up {{ provisioning_net }}
 {% endif %}
