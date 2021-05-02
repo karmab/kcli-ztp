6b5dfa4af71f3dddb62b81861277c2324327354e
karmab <karimboumedhel@gmail.com>
2021-05-02 10:41:33 +0200

    hack 3 masters usecase

--- 09_deploy_openshift.sh
+++ 09_deploy_openshift.sh
@@ -14,7 +14,9 @@ python3 /root/bin/ipmi.py off
 python3 /root/bin/redfish.py off
 cp install-config.yaml ocp
 openshift-baremetal-install --dir ocp --log-level debug create manifests
-cp manifests/*y*ml >/dev/null 2>&1 || true
+cp manifests/*y*ml >/dev/null 2>&1 ocp/openshift || true
+TOTAL_WORKERS=$(grep 'role: worker' /root/install-config.yaml | wc -l)
+[ "$TOTAL_WORKERS" -gt "0" ] || cp 99-openshift-ingress-controller-master.yaml ocp/openshift
 echo {{ api_ip }} api.{{ cluster }}.{{ domain }} >> /etc/hosts
 openshift-baremetal-install --dir ocp --log-level debug create cluster || true
 openshift-baremetal-install --dir ocp --log-level debug wait-for install-complete || openshift-baremetal-install --dir ocp --log-level debug wait-for install-complete
@@ -24,11 +26,10 @@ for node in $(oc get nodes --selector='node-role.kubernetes.io/master' -o name) 
 done
 {% endif %}
 export OS_CLOUD=metal3
-TOTAL_WORKERS=$(grep 'role: worker' /root/install-config.yaml | wc -l)
 if [ "$TOTAL_WORKERS" -gt "0" ] ; then
  until [ "$CURRENT_WORKERS" == "$TOTAL_WORKERS" ] ; do
   CURRENT_WORKERS=$(oc get nodes --selector='node-role.kubernetes.io/worker' -o name | wc -l)
-  logger "Waiting for workers to all show up..."
+  logger "Waiting for all workers to show up..."
   sleep 5
 done
 fi
