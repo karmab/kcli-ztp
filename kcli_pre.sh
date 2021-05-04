9d2d15885916cf1bf728e6f07c0a5a68586a0bee
karmab <karimboumedhel@gmail.com>
2021-05-04 13:12:14 +0200

    skip network test in lab mode

--- kcli_pre.sh
+++ kcli_pre.sh
@@ -5,6 +5,6 @@ echo $DOTS
 {% elif version == 'ci' %}
 grep -q registry.ci.openshift.org {{ pullsecret }} || (echo Missing token for registry.ci.openshift.org && exit 1)
 {% endif %}
-{% if config_host == '127.0.0.1' %}
+{% if config_host == '127.0.0.1' and not lab %}
 ip a l {{ baremetal_net }} >/dev/null 2>&1 || (echo Issue with network {{ baremetal_net }} && exit 1)
 {% endif %}
