f5b45b631ed3f9141efbb380d4ebee80d2c0e731
karmab <karimboumedhel@gmail.com>
2021-06-03 10:33:15 +0200

    typo in pre

--- kcli_pre.sh
+++ kcli_pre.sh
@@ -7,7 +7,7 @@ DOTS=$(echo {{ tag }} | grep -o '\.' | wc -l)
 [ "$DOTS" -eq "1" ] || (echo tag should be 4.X && exit 1)
 {% if version in ['latest', 'stable'] %}
 VERSIONCHECK=$(curl -s https://mirror.openshift.com/pub/openshift-v4/clients/ocp/{{ version }}-{{ tag }}/release.txt | grep -q 'Pull from')
-[ "$VERSIONCHECK" -eq "0" ] || (echo incorrect mix {{ version }} and {{ tag }} && exit 1)
+[ "$VERSIONCHECK" == "" ] || (echo incorrect mix {{ version }} and {{ tag }} && exit 1)
 {% endif %}
 {% elif version == 'ci' %}
 grep -q registry.ci.openshift.org {{ pullsecret }} || (echo Missing token for registry.ci.openshift.org && exit 1)
