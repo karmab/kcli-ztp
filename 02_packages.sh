8b1f78e0aa8ca3c1016937db28bf9a92095237fb
Yuval Kashtan <yuvalkashtan@gmail.com>
2021-08-18 16:35:39 +0300

    add time observability for lengthy operations

--- 02_packages.sh
+++ 02_packages.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-dnf -y install libvirt-libs libvirt-client ipmitool mkisofs tmux make git bash-completion
+time dnf -y install libvirt-libs libvirt-client ipmitool mkisofs tmux make git bash-completion
 dnf -y install python36
 export CRYPTOGRAPHY_DONT_BUILD_RUST=1
 pip3 install -U pip
