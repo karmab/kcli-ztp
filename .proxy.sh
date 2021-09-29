6845ffd2c52b3bd006bf6b44e4ea2fc15a0f375d
karmab <karimboumedhel@gmail.com>
2021-09-29 18:50:53 +0200

    additional proxy tricks

---
+++ .proxy.sh
@@ -0,0 +1,8 @@
+echo "proxy={{ 'http://' + http_proxy if 'http' not in http_proxy else http_proxy }}" >> /etc/dnf/dnf.conf
+while true ; do
+  IP=$(ip -o addr show {{ installer_nic }} |head -1 | awk '{print $4}' | cut -d'/' -f1)
+  echo $IP | grep -q '\.' && break
+  sleep 5
+done
+sed -i "s@noProxy: {{ baremetal_cidr }}@noProxy: $IP,.{{ cluster }}.{{ domain }}@" /root/install-config.yaml
+sed -i "s@NO_PROXY={{ baremetal_cidr }}@NO_PROXY=$IP,.{{ cluster }}.{{ domain }}@" /etc/profile.d/proxy.sh
