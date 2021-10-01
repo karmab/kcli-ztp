da88b8c518580b2be6bd4848f3581593fc099233
karmab <karimboumedhel@gmail.com>
2021-10-01 15:21:38 +0200

    fix conditionals in kcli_pre.sh script

--- kcli_pre.sh
+++ kcli_pre.sh
@@ -5,14 +5,14 @@ exit 1
 {% endif %}
 
 {% if config_host == '127.0.0.1' and not lab %}
-ip a l {{ baremetal_net }} >/dev/null 2>&1 || (echo Issue with network {{ baremetal_net }} && exit 1)
+ip a l {{ baremetal_net }} >/dev/null 2>&1 || {echo Issue with network {{ baremetal_net }} ; exit 1;}
 {% endif %}
 
 # VERSION CHECK
 {% if version is defined %}
 {% if version in ['latest', 'stable'] %}
 DOTS=$(echo {{ tag }} | grep -o '\.' | wc -l)
-[ "$DOTS" -eq "1" ] || (echo tag should be 4.X && exit 1)
+[ "$DOTS" -eq "1" ] || {echo tag should be 4.X ; exit 1;}
 curl -Ns https://mirror.openshift.com/pub/openshift-v4/clients/ocp/{{ version }}-{{ tag }}/release.txt | grep -q 'Pull From'
 if  [ "$?" != "0" ] ; then 
   echo incorrect mix {{ version }} and {{ tag }}
@@ -27,7 +27,7 @@ if [ "$?" != "0" ] ; then
   exit 1
 fi
 {% elif version == 'ci' %}
-grep -q registry.ci.openshift.org {{ pullsecret }} || (echo Missing token for registry.ci.openshift.org && exit 1)
+grep -q registry.ci.openshift.org {{ pullsecret }} || {echo Missing token for registry.ci.openshift.org ; exit 1;}
 {% endif %}
 {% endif %}
 
