5ad10bab72e3a12803f93e60759b51a74103f57f
karmab <karimboumedhel@gmail.com>
2021-10-27 13:21:45 +0200

    install vim

--- 02_packages.sh
+++ 02_packages.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-time dnf -y install libvirt-libs libvirt-client ipmitool mkisofs tmux make git bash-completion
+time dnf -y install libvirt-libs libvirt-client ipmitool mkisofs tmux make git bash-completion vim-enhanced
 dnf -y install python36
 export CRYPTOGRAPHY_DONT_BUILD_RUST=1
 pip3 install -U pip
