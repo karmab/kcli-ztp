80a923d4b563f7cead84013a0097bb70146beb8c
karmab <karimboumedhel@gmail.com>
2022-01-04 17:21:16 +0100

    full support for static networking

--- 09_deploy_openshift.sh
+++ 09_deploy_openshift.sh
@@ -20,6 +20,13 @@ cp manifests/*y*ml >/dev/null 2>&1 ocp/openshift || true
 TOTAL_WORKERS=$(grep 'role: worker' /root/install-config.yaml | wc -l) || true
 # [ "$TOTAL_WORKERS" -gt "0" ] || cp 99-openshift-ingress-controller-master.yaml ocp/openshift
 echo {{ api_ip }} api.{{ cluster }}.{{ domain }} >> /etc/hosts
+{% if baremetal_bootstrap_ip != None %}
+openshift-baremetal-install --dir ocp --log-level debug create ignition-configs
+NIC=ens3
+NICDATA="$(cat /root/ifcfg-bootstrap | base64 -w0)"
+cp /root/ocp/bootstrap.ign /root/ocp/bootstrap.ign.ori
+cat /root/ocp/bootstrap.ign.ori | jq ".storage.files |= . + [{\"filesystem\": \"root\", \"mode\": 420, \"path\": \"/etc/sysconfig/network-scripts/ifcfg-$NIC\", \"contents\": {\"source\": \"data:text/plain;charset=utf-8;base64,$NICDATA\", \"verification\": {}}}]" > /root/ocp/bootstrap.ign
+{% endif %}
 openshift-baremetal-install --dir ocp --log-level debug create cluster || true
 openshift-baremetal-install --dir ocp --log-level debug wait-for install-complete || openshift-baremetal-install --dir ocp --log-level debug wait-for install-complete
 {% if virtual_masters %}
