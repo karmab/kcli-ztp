bc56d3738bf9061c7c9b221a32493eda1900696b
karmab <karimboumedhel@gmail.com>
2022-02-02 16:32:44 +0100

    cleaner clean.sh

--- helpers/clean.sh
+++ helpers/clean.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env bash
 
 [ -d /root/ocp ] && rm -rf /root/ocp
-export LIBVIRT_DEFAULT_URI=qemu+ssh://{{ config_user | default('root') }}@{{ config_host if config_host != '127.0.0.1' else baremetal_net|local_ip(true) }}/system
-cluster={{ cluster }}
-bootstrap=$(virsh list --all --name | grep "$cluster.*bootstrap")
-if [ "$bootstrap" != "" ] ; then
-  for vm in $bootstrap ; do
-   echo "Deleting old bootstrap vm $vm"
-   virsh destroy $vm
-   virsh undefine $vm
+CLUSTER={{ cluster }}
+HYPERVISOR={{ config_user | default('root') }}@{{ config_host if config_host != '127.0.0.1' else baremetal_net|local_ip(true) }}
+export LIBVIRT_DEFAULT_URI=qemu+ssh://$HYPERVISOR/system
+BOOTSTRAP=$(virsh list --all --name | grep "$CLUSTER.*bootstrap")
+if [ "$BOOTSTRAP" != "" ] ; then
+  for VM in $BOOTSTRAP ; do
+   echo "Deleting old bootstrap vm $VM"
+   virsh destroy $VM
+   virsh undefine $VM
   done
 fi
 
-pools=$(virsh pool-list --all --name | grep "$cluster.*bootstrap")
-if [ "$pools" != "" ] ; then
-  for pool in $pools ; do
-    echo "Handling old assets for pool $pool"
-    virsh vol-delete $pool $pool
-    virsh vol-delete $pool-base $pool
-    virsh vol-delete $pool.ign $pool
-    virsh pool-destroy $pool
-    virsh pool-undefine $pool
-    ssh {{ config_user | default('root') }}@{{ config_host if config_host != '127.0.0.1' else baremetal_net|local_ip }} "rmdir /var/lib/libvirt/openshift-images/$pool"
+POOLS=$(virsh pool-list --all --name | grep "$CLUSTER.*bootstrap")
+if [ "$POOLS" != "" ] ; then
+  for POOL in $POOLS ; do
+    echo "Handling old assets for pool $POOL"
+    virsh vol-delete $POOL $POOL
+    virsh vol-delete $POOL-base $POOL
+    virsh vol-delete $POOL.ign $POOL
+    virsh pool-destroy $POOL
+    virsh pool-undefine $POOL
+    ssh $HYPERVISOR "rmdir /var/lib/libvirt/openshift-images/$POOL"
   done
 fi
 
-ssh {{ config_user | default('root') }}@{{ config_host if config_host != '127.0.0.1' else baremetal_net|local_ip }} "find /var/lib/libvirt/images/boot-* -type f -mtime +5 -exec virsh vol-delete {} +"
+ssh $HYPERVISOR "find /var/lib/libvirt/images/boot-* -type f -mtime +5 -exec virsh vol-delete {} +"
