62a2ebfb93c1fab7edade0bbba1ea2cc67bda94d
karmab <karimboumedhel@gmail.com>
2022-03-18 13:01:30 +0100

    add missing oauth file

--- scripts/11_apps.sh
+++ scripts/11_apps.sh
@@ -13,7 +13,7 @@ echo "Adding dev user $DEV_USER with password $DEV_PASSWORD"
 echo "Adding admin user $ADMIN_USER with password $ADMIN_PASSWORD"
 printf "$ADMIN_USER:$(openssl passwd -apr1 $ADMIN_PASSWORD )\n$DEV_USER:$(openssl passwd -apr1 $DEV_PASSWORD )\n" > htpasswd
 oc create secret generic htpass-secret --from-file=htpasswd=htpasswd -n openshift-config
-oc apply -f oauth.yml
+oc apply -f /root/scripts/oauth.yml
 echo "Granting cluster-admin role to $ADMIN_USER"
 oc adm policy add-cluster-role-to-user cluster-admin $ADMIN_USER
 {% endif %}
