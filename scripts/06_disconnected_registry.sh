c55dfafa4e1f81e53155c8a26bdb43467b083af4
karmab <karimboumedhel@gmail.com>
2022-04-08 18:04:56 +0200

    add libseccomp-devel for podman

--- scripts/06_disconnected_registry.sh
+++ scripts/06_disconnected_registry.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 PRIMARY_NIC=$(ls -1 /sys/class/net | head -1)
 export PATH=/root/bin:$PATH
 export PULL_SECRET="/root/openshift_pull.json"
-dnf -y install podman httpd httpd-tools jq skopeo
+dnf -y install podman httpd httpd-tools jq skopeo libseccomp-devel
 export IP=$(ip -o addr show $PRIMARY_NIC | head -1 | awk '{print $4}' | cut -d'/' -f1)
 REGISTRY_NAME=$(echo $IP | sed 's/\./-/g' | sed 's/:/-/g').sslip.io
 REGISTRY_USER={{ disconnected_user }}
