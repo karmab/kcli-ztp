e9fb9566e71c1a57a888505e8ff835074a798e99
Eduardo Minguez Perez <e.minguez@gmail.com>
2022-04-20 14:33:34 +0200

    Patch the argocd route to reencrypt

--- ztp/scripts/argocd.sh
+++ ztp/scripts/argocd.sh
@@ -1,3 +1,10 @@
 cd /root/ztp/argocd
 oc patch argocd openshift-gitops -n openshift-gitops --type=merge --patch-file argocd-openshift-gitops-patch.json
+
+# Patch the argocd route to use reencrypt because a known issue
+# https://github.com/redhat-developer/gitops-operator/issues/261
+# https://issues.redhat.com/browse/GITOPS-1548 
+oc patch argocd -n openshift-gitops openshift-gitops --type='merge' \
+  -p '{"spec":{"server":{"route":{"tls":{"termination":"reencrypt"}}}}}'
+  
 oc apply -k .
