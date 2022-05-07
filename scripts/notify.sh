3bdb3738c8038415d0eaede92c50705b5e8d517d
karmab <karimboumedhel@gmail.com>
2022-05-07 09:31:40 +0200

    improve notify script

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -4,18 +4,21 @@ set -euo pipefail
 
 export KUBECONFIG=/root/ocp/auth/kubeconfig
 echo "Report from vm: $(hostname) ip: $(hostname -I | cut -d' ' -f1)"
+echo "Update time: $(uptime | awk '{ print $3 }' | sed 's/,//')"
 echo "Cluster info:"
 oc get clusterversion
 echo "Nodes info:"
 oc get nodes
-echo "Update time:"
-uptime | awk '{ print $3 }' | sed 's/,//'
 {% if ztp_spokes is defined %}
+echo -e "\nSpokes:\n"
 {% for spoke in ztp_spokes %}
 export SPOKE={{ spoke.name }}
+echo "Spoke $SPOKE"
+echo "Agents:"
 oc get agent -A
-oc get agentclusterinstall -n $SPOKE $SPOKE -o jsonpath={'.status.conditions[-1].message'}
+echo "Cluster State:"
 oc get agentclusterinstall -n $SPOKE $SPOKE -o jsonpath={'.status.debugInfo.state'}
+echo "Cluster Info:"
 oc get agentclusterinstall -n $SPOKE $SPOKE -o jsonpath={'.status.debugInfo.stateInfo'}
 {% endfor%}
 {% endif %}
