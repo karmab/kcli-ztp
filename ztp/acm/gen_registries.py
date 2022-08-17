57729083f62e27e2ffead1f2312dece19a2f9db7
Mario Vázquez <9841873+mvazquezc@users.noreply.github.com>
2022-08-17 16:52:17 +0200

    Added support for multiple icsps in the same file (#89)
    Signed-off-by: Mario Vazquez <mavazque@redhat.com>
    
    Signed-off-by: Mario Vazquez <mavazque@redhat.com>

--- ztp/acm/gen_registries.py
+++ ztp/acm/gen_registries.py
@@ -6,18 +6,17 @@ import yaml
 manifestfile = glob.glob('/root/manifests-redhat-operator-index-*/imageContentSourcePolicy.yaml')[0]
 results = ''
 with open(manifestfile) as f:
-    data = yaml.safe_load(f)
-
-mirrors = data['spec']['repositoryDigestMirrors']
-for mirror in mirrors:
-    registry1 = mirror['source']
-    registry2 = mirror['mirrors'][0]
-    results += """\n    [[registry]]
+    data = yaml.safe_load_all(f)
+    for d in data:
+        mirrors = d['spec']['repositoryDigestMirrors']
+        for mirror in mirrors:
+            registry1 = mirror['source']
+            registry2 = mirror['mirrors'][0]
+            results += """\n    [[registry]]
     prefix = ""
     location = "{registry1}"
     mirror-by-digest-only = true
 
     [[registry.mirror]]
       location = "{registry2}"\n""".format(registry1=registry1, registry2=registry2)
-
 print(results)
