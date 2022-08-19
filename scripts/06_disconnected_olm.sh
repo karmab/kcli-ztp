99c0937253d34046e8264fbac787d0504cbc5a1c
Karim Boumedhel <1840387+karmab@users.noreply.github.com>
2022-08-19 12:33:28 +0200

    switch to oc-mirror (#91)

--- scripts/06_disconnected_olm.sh
+++ scripts/06_disconnected_olm.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-# Variables to set, suit to your installation
-export RH_OP_PACKAGES=${RH_OP_PACKAGES:-{{ disconnected_operators|join(",") }}}
-export CERT_OP_PACKAGES=${CERT_OP_PACKAGES:-{{ disconnected_certified_operators|join(",") }}}
-export COMMUNITY_OP_PACKAGES=${COMMUNITY_OP_PACKAGES:-{{ disconnected_community_operators|join(",") }}}
-export MARKETPLACE_OP_PACKAGES=${MARKETPLACE_OP_PACKAGES:-{{ disconnected_marketplace_operators|join(",") }}}
-if [ -z $RH_OP_PACKAGES ] && [ -z $CERT_OP_PACKAGES ] [ -z $COMMUNITY_OP_PACKAGES ] && [ -z $MARKETPLACE_OP_PACKAGES ]; then
- echo You need one of the following env variables at least: RH_OP_PACKAGES CERT_OP_PACKAGES COMMUNITY_OP_PACKAGES MARKETPLACE_OP_PACKAGES
- exit 1
-fi
 cd /root
 export PATH=/root/bin:$PATH
 export OCP_RELEASE="$(/root/bin/openshift-baremetal-install version | head -1 | cut -d' ' -f2 | cut -d'.' -f 1,2)"
@@ -42,53 +33,18 @@ RHN_USER=$(echo $REDHAT_CREDS | cut -d: -f1)
 RHN_PASSWORD=$(echo $REDHAT_CREDS | cut -d: -f2)
 podman login -u "$RHN_USER" -p "$RHN_PASSWORD" registry.redhat.io
 
-which opm >/dev/null 2>&1
+which oc-mirror >/dev/null 2>&1
 if [ "$?" != "0" ] ; then
-curl -k -s https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/latest/opm-linux.tar.gz | tar xvz -C /usr/bin/
-chmod u+x /usr/bin/opm
+  curl -sL https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/latest/oc-mirror.tar.gz | tar xvz -C /usr/bin
+  chmod +x /usr/bin/oc-mirror
 fi
 
-export RH_OP_INDEX="registry.redhat.io/redhat/redhat-operator-index:v${OCP_RELEASE}"
-export CERT_OP_INDEX="registry.redhat.io/redhat/certified-operator-index:v${OCP_RELEASE}"
-export COMM_OP_INDEX="registry.redhat.io/redhat/community-operator-index:v${OCP_RELEASE}"
-export MARKETPLACE_OP_INDEX="registry.redhat.io/redhat-marketplace-index:v${OCP_RELEASE}"
+mkdir -p /root/.docker
+cp -f /root/openshift_pull.json /root/.docker/config.json
 
-if [ ! -z $RH_OP_PACKAGES ] ; then
- rm -rf manifests-redhat-operator-index-* || true
- export RH_INDEX_TAG=olm-index/redhat-operator-index:v$OCP_RELEASE
- time opm index prune --from-index $RH_OP_INDEX --packages $RH_OP_PACKAGES --tag $LOCAL_REGISTRY/$RH_INDEX_TAG
- podman push $LOCAL_REGISTRY/$RH_INDEX_TAG --authfile $OCP_PULLSECRET_AUTHFILE
- time oc adm catalog mirror $LOCAL_REGISTRY/$RH_INDEX_TAG $LOCAL_REGISTRY/$IMAGE_TAG --registry-config=$OCP_PULLSECRET_AUTHFILE --max-per-registry=100
- oc apply -f /root/manifests-redhat-operator-index-*/imageContentSourcePolicy.yaml 2>/dev/null || cp /root/manifests-redhat-operator-index-*/imageContentSourcePolicy.yaml /root/manifests/redhat-imageContentSourcePolicy.yaml
- oc apply -f /root/manifests-redhat-operator-index-*/catalogSource.yaml 2>/dev/null || cp /root/manifests-redhat-operator-index-*/catalogSource.yaml /root/manifests/redhat-catalogSource.yaml
-fi
+envsubst < /root/scripts/mirror-config.yaml.sample > /root/mirror-config.yaml
 
-if [ ! -z $CERT_OP_PACKAGES ] ; then
- rm -rf manifests-certified-operator-index-* || true
- export CERT_INDEX_TAG=olm-index/certified-operator-index:v$OCP_RELEASE
- time opm index prune --from-index $CERT_OP_INDEX --packages $CERT_OP_PACKAGES --tag $LOCAL_REGISTRY/$CERT_INDEX_TAG
- podman push $LOCAL_REGISTRY/$CERT_INDEX_TAG --authfile $OCP_PULLSECRET_AUTHFILE
- time oc adm catalog mirror $LOCAL_REGISTRY/$CERT_INDEX_TAG $LOCAL_REGISTRY/$IMAGE_TAG --registry-config=$OCP_PULLSECRET_AUTHFILE --max-per-registry=100
- oc apply -f /root/manifests-certified-operator-index-*/imageContentSourcePolicy.yaml 2>/dev/null || cp /root/manifests-certified-operator-index-*/imageContentSourcePolicy.yaml /root/manifests/certified-imageContentSourcePolicy.yaml
- oc apply -f /root/manifests-certified-operator-index-*/catalogSource.yaml 2>/dev/null || cp /root/manifests-certified-operator-index-*/catalogSource.yaml /root/manifests/certified-catalogSource.yaml
-fi
-
-if [ ! -z $COMM_OP_PACKAGES ] ; then
- rm -rf manifests-community-operator-index-* || true
- export COMM_INDEX_TAG=olm-index/community-operator-index:v$OCP_RELEASE
- time opm index prune --from-index $COMM_OP_INDEX --packages $COMM_OP_PACKAGES --tag $LOCAL_REGISTRY/$COMM_INDEX_TAG
- podman push $LOCAL_REGISTRY/$COMM_INDEX_TAG --authfile $OCP_PULLSECRET_AUTHFILE
- time oc adm catalog mirror $LOCAL_REGISTRY/$COMM_INDEX_TAG $LOCAL_REGISTRY/$IMAGE_TAG --registry-config=$OCP_PULLSECRET_AUTHFILE --max-per-registry=100
- oc apply -f /root/manifests-community-operator-index-*/imageContentSourcePolicy.yaml 2>/dev/null || cp /root/manifests-community-operator-index-*/imageContentSourcePolicy.yaml /root/manifests/community-imageContentSourcePolicy.yaml
- oc apply -f /root/manifests-community-operator-index-*/catalogSource.yaml 2>/dev/null || cp /root/manifests-community-operator-index-*/catalogSource.yaml /root/manifests/community-catalogSource.yaml
-fi
-
-if [ ! -z $MARKETPLACE_OP_PACKAGES ] ; then
- rm -rf manifests-redhat-marketplace-operator-index-* || true
- export MARKETPLACE_INDEX_TAG=olm-index/redhat-marketplace-operator-index:v$OCP_RELEASE
- time opm index prune --from-index $MARKETPLACE_OP_INDEX --packages $MARKETPLACE_OP_PACKAGES --tag $LOCAL_REGISTRY/$MARKETPLACE_INDEX_TAG
- podman push $LOCAL_REGISTRY/$MARKETPLACE_INDEX_TAG --authfile $OCP_PULLSECRET_AUTHFILE
- time oc adm catalog mirror $LOCAL_REGISTRY/$MARKETPLACE_INDEX_TAG $LOCAL_REGISTRY/$IMAGE_TAG --registry-config=$OCP_PULLSECRET_AUTHFILE --max-per-registry=100
- oc apply -f /root/manifests-redhat-marketplace-operator-index-*/imageContentSourcePolicy.yaml 2>/dev/null || cp /root/manifests-redhat-marketplace-operator-index-*/imageContentSourcePolicy.yaml /root/manifests/redhat-marketplace-imageContentSourcePolicy.yaml
- oc apply -f /root/manifests-redhat-marketplace-operator-index-*/catalogSource.yaml 2>/dev/null || cp /root/manifests-redhat-marketplace-operator-index-*/catalogSource.yaml /root/manifests/redhat-marketplace-catalogSource.yaml
-fi
+rm -rf /root/oc-mirror-workspace || true
+oc-mirror --config /root/mirror-config.yaml docker://$LOCAL_REGISTRY
+oc apply -f /root/oc-mirror-workspace/results-*/imageContentSourcePolicy.yaml 2>/dev/null || cp /root/oc-mirror-workspace/results-*/imageContentSourcePolicy.yaml /root/manifests
+oc apply -f /root/oc-mirror-workspace/results-*/catalogSource* 2>/dev/null || cp /root/oc-mirror-workspace/results-*/catalogSource* /root/manifests
