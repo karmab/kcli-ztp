66488e73d18082295f1ce2f27ecfc09becb55d65
karmab <karimboumedhel@gmail.com>
2022-11-15 18:21:22 +0100

    fix nfs script typos

--- scripts/10_nfs.sh
+++ scripts/10_nfs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# set -euo pipefail
+set -euo pipefail
 
 PRIMARY_NIC=$(ls -1 /sys/class/net | grep 'eth\|en' | head -1)
 export KUBECONFIG=/root/ocp/auth/kubeconfig
@@ -22,7 +22,7 @@ oc project $NAMESPACE
 sed -i "s/namespace:.*/namespace: $NAMESPACE/g" $BASEDIR/deploy/rbac.yaml $BASEDIR/deploy/deployment.yaml
 oc create -f $BASEDIR/deploy/rbac.yaml
 oc adm policy add-scc-to-user hostmount-anyuid system:serviceaccount:$NAMESPACE:nfs-client-provisioner
-if [ "$(podman ps | grep -q registry)" == "0" ] ; then
+if [ "$(podman ps | grep registry)" != "" ] ; then
  /root/bin/sync_image.sh k8s.gcr.io/sig-storage/nfs-subdir-external-provisioner:v4.0.2
  REGISTRY_NAME=$(echo $PRIMARY_IP | sed 's/\./-/g' | sed 's/:/-/g').sslip.io
  sed -i "s@k8s.gcr.io@$REGISTRY_NAME:5000@" $BASEDIR/deploy/deployment.yaml
@@ -30,8 +30,8 @@ fi
 sed -i -e "s@k8s-sigs.io/nfs-subdir-external-provisioner@storage.io/nfs@" -e "s@10.3.243.101@$PRIMARY_IP@" -e "s@/ifs/kubernetes@/var/nfsshare@" $BASEDIR/deploy/deployment.yaml
 echo 'apiVersion: storage.k8s.io/v1
 kind: StorageClass
-    metadata:
-  name: nfs
+metadata:
+   name: nfs
 provisioner: storage.io/nfs
 parameters:
   pathPattern: "${.PVC.namespace}/${.PVC.name}"
