c6c38bf52099db76e60c3b0de1b67602bc9291b4
karmab <karimboumedhel@gmail.com>
2022-11-18 11:30:32 +0100

    ztp: only warn when multinode cluster lacks api/ingress vip

--- kcli_pre.sh
+++ kcli_pre.sh
@@ -114,10 +114,10 @@ echo Incorrect spoke_name {{ spoke_name }}: cant contain an underscore && exit 1
 {% endif %}
 {% if spoke_masters_number > 1 %}
 {% if spoke_api_ip == None %}
-echo spoke_api_ip needs to be set if deploying an HA spoke && exit 1
+echo no spoke_api_ip. You will have to handle this by yourself
 {% endif %}
 {% if spoke_ingress_ip == None %}
-echo spoke_ingress_ip needs to be set if deploying an HA spoke && exit 1
+echo no spoke_ingress_ip. You will have to handle this by yourself
 {% endif %}
 {% endif %}
 {% endfor %}
