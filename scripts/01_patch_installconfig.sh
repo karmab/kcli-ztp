8d59176dc3470f8ac8b3ed1868c7e1365758afa5
karmab <karimboumedhel@gmail.com>
2022-11-21 17:32:12 +0100

    create manifests dir if missing

--- scripts/01_patch_installconfig.sh
+++ scripts/01_patch_installconfig.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+[ -d /root/manifests ] || mkdir -p /root/manifests
 ssh-keyscan -H {{ config_host if config_host not in ['127.0.0.1', 'localhost'] else baremetal_net|local_ip }} >> ~/.ssh/known_hosts
 echo -e "Host=*\nStrictHostKeyChecking=no\n" > ~/.ssh/config
 
