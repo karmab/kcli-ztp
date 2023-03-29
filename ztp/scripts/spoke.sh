96065086ecdee41c8c0fb90204ee2c9aa0100bd6
Karim Boumedhel <1840387+karmab@users.noreply.github.com>
2023-03-29 21:47:12 +0200

    ztp: contrail support (#96)

--- ztp/scripts/spoke.sh
+++ ztp/scripts/spoke.sh
@@ -7,6 +7,7 @@
 {% set spoke_ctlplanes_number = spoke.get('ctlplanes_number', 1) %}
 {% set spoke_workers_number = spoke.get('workers_number', 0) %}
 {% set spoke_deploy = spoke.get('deploy', True) %}
+{% set spoke_network_type = spoke.get('network_type', ztp_spoke_network_type) %}
 
 echo export SPOKE={{ spoke_name }} >> /root/.bashrc
 {% if spoke_ctlplanes_number > 1 and spoke_api_ip != None and spoke_ingress_ip != None %}
@@ -17,11 +18,15 @@ export MINOR=$(echo $OCP_RELEASE | cut -d. -f1,2)
 export PULLSECRET=$(cat /root/openshift_pull.json | tr -d [:space:])
 export SPOKE={{ spoke_name }}
 export DOMAIN={{ domain }}
+
 export MASTERS_NUMBER={{ spoke_ctlplanes_number }}
 export WORKERS_NUMBER={{ spoke_workers_number }}
 export SSH_PUB_KEY=$(cat /root/.ssh/id_rsa.pub)
-[ -d /root/spoke_$SPOKE/manifests ] && bash /root/spoke_$SPOKE/manifests.sh
-envsubst < /root/spoke_$SPOKE/spoke.sample.yml > /root/spoke_$SPOKE/spoke.yml
+
+[ -f /root/spoke_$SPOKE/contrail.sh ] && /root/spoke_$SPOKE/contrail.sh
+
+[ -d /root/spoke_$SPOKE/manifests ] && /root/spoke_$SPOKE/manifests.sh
+envsubst < /root/spoke_$SPOKE/spoke.sample.yml >> /root/spoke_$SPOKE/spoke.yml
 
 {% if spoke_deploy %}
 oc create namespace $SPOKE
