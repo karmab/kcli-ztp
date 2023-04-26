0cdab26571acf61feeaabf216c1d3066f780cb87
karmab <karimboumedhel@gmail.com>
2023-04-26 18:30:13 +0200

    fix 10_apps.sh for users

--- scripts/10_apps.sh
+++ scripts/10_apps.sh
@@ -4,5 +4,9 @@ set -euo pipefail
 export PYTHONUNBUFFERED=true
 
 {% for app in apps %}
+{% if app == 'users' %}
+kcli create app openshift {{ app }} -P users_admin={{ users_admin }} -P users_adminpassword={{ users_adminpassword }} -P users_dev={{ users_dev }} -P users_devpassword={{ users_devpassword }}
+{% else %}
 kcli create app openshift {{ app }} -P install_cr=false
+{% endif %}
 {% endfor %}
