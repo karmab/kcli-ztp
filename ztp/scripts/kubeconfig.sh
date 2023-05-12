1c851cf3c9acfab4b183fd425a66bf5a4c11fffa
karmab <karimboumedhel@gmail.com>
2023-05-12 19:23:25 +0200

    bump contrail version

--- ztp/scripts/kubeconfig.sh
+++ ztp/scripts/kubeconfig.sh
@@ -2,3 +2,5 @@
 
 SPOKE={{ spoke }}
 oc get secret -n $SPOKE $SPOKE-admin-kubeconfig -o jsonpath='{.data.kubeconfig}' | base64 -d > /root/kubeconfig.$SPOKE
+echo Use the following to connect to this SPOKE
+echo export KUBECONFIG=/root/kubeconfig.$SPOKE
