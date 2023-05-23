9c15ddf886015089d17638b32f3c626b3ce3254b
karmab <karimboumedhel@gmail.com>
2023-05-23 12:55:50 +0200

    allow to parse parameter file when installing apps

--- scripts/10_apps.sh
+++ scripts/10_apps.sh
@@ -6,6 +6,8 @@ export PYTHONUNBUFFERED=true
 {% for app in apps %}
 {% if app == 'users' %}
 kcli create app openshift {{ app }} -P users_admin={{ users_admin }} -P users_adminpassword={{ users_adminpassword }} -P users_dev={{ users_dev }} -P users_devpassword={{ users_devpassword }}
+{% elif storemetadata|default(False) %}
+kcli create app openshift {{ app }} --pf /root/.metadata
 {% else %}
 kcli create app openshift {{ app }} -P install_cr=false
 {% endif %}
