edaa553fb2fec08d1d73759bb2aed0f856cefca6
karmab <karimboumedhel@gmail.com>
2023-10-03 20:33:29 +0200

    ztp siteconfig: log in registry.redhat.io

--- ztp/scripts/siteconfig.sh
+++ ztp/scripts/siteconfig.sh
@@ -11,6 +11,10 @@ envsubst < /root/ztp/scripts/siteconfig.sample.yml > /root/ztp/scripts/siteconfi
 
 mkdir -p /root/.config/kustomize/plugin
 export HOME=/root
+REDHAT_CREDS=$(cat /root/openshift_pull.json | jq .auths.\"registry.redhat.io\".auth -r | base64 -d)
+RHN_USER=$(echo $REDHAT_CREDS | cut -d: -f1)
+RHN_PASSWORD=$(echo $REDHAT_CREDS | cut -d: -f2)
+podman login -u "$RHN_USER" -p "$RHN_PASSWORD" registry.redhat.io
 podman cp $(podman create --name policgentool --rm registry.redhat.io/openshift4/ztp-site-generate-rhel8:v$MINOR):/kustomize/plugin/ran.openshift.io /root/.config/kustomize/plugin/
 
 oc kustomize /root/ztp/scripts --enable-alpha-plugins >> /root/spokes.yml
