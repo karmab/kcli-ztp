f7e0a0408673f5ec7b07ab70729bea5502fd6862
karmab <karimboumedhel@gmail.com>
2024-03-14 15:55:50 +0100

    delete bootstrap vm in clean script

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+CLUSTER={{ cluster }}
 [ -d /root/ocp ] && rm -rf /root/ocp
+VMS=$(kcli list vm | grep ${CLUSTER}-.*-bootstrap | cut -d"|" -f 2 | xargs)
+[ -z "$VMS" ] || kcli delete vm --yes $VMS
