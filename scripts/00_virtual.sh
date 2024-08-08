852f3d8cf972d182b000fedc2bfbf795db324494
karmab <karimboumedhel@gmail.com>
2024-08-08 15:52:17 +0200

    add support beyond kvm

--- scripts/00_virtual.sh
+++ scripts/00_virtual.sh
@@ -12,6 +12,10 @@ dnf -y install pkgconf-pkg-config gcc python3-libvirt python3 git python3-netifa
 
 dnf -y copr enable karmab/kcli
 dnf -y install kcli
+{% if config_type != 'kvm' %}
+dnf -y install epel-release
+kcli install provider {{ config_type }}
+{% endif %}
 
 SUSHYFLAGS={{ "--ipv6" if ':' in baremetal_cidr else "" }}
 kcli create sushy-service $SUSHYFLAGS
