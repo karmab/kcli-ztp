1509de5d29e4f0671a11dd0dea5b0fcc03f70de6
karmab <karimboumedhel@gmail.com>
2024-08-10 15:56:11 +0200

    support sno+worker

--- ztp/scripts/spokes_deploy.sh
+++ ztp/scripts/spokes_deploy.sh
@@ -25,7 +25,7 @@ done
 if [ "$installed" == "true" ] ; then
  echo "Cluster $SPOKE deployed"
  oc get secret -n $SPOKE $SPOKE-admin-kubeconfig -o jsonpath='{.data.kubeconfig}' | base64 -d > /root/kubeconfig.$SPOKE
- {% if spoke_ctlplanes_number == 1 and spoke_workers_number == 0 %}
+ {% if spoke_ctlplanes_number == 1 %}
  {% if ':' in baremetal_cidr %}
  SNO_IP=$(oc get -n $SPOKE $(oc get agent -n $SPOKE -o name) -o jsonpath={'.status.inventory.interfaces[0].ipV6Addresses[0]'} | cut -d/ -f1)
  {% else %}
@@ -38,6 +38,28 @@ elif [ "$failed" == "true" ] ; then
  echo message: $MSG
 else
  echo Timeout waiting for spoke cluster $SPOKE to be deployed
+fi
+
+# SNOPLUS HANDLING
+if [ -f /root/ztp/scripts/extra_bmc_$SPOKE.yml ] ; then
+ BAREMETAL_IP=$(ip -o addr show eth0 | head -1 | awk '{print $4}' | cut -d'/' -f1)
+ echo $BAREMETAL_IP | grep -q ':' && BAREMETAL_IP=[$BAREMETAL_IP]
+ sed -i s/CHANGEME/$BAREMETAL_IP/ /root/ztp/scripts/extra_bmc_$SPOKE.yml
+ oc create -f /root/ztp/scripts/extra_bmc_$SPOKE.yml
+fi
+timeout=0
+installed=false
+while [ "$timeout" -lt "{{ spoke_wait_time }}" ] ; do
+  AGENTS=$(oc get agent -n $SPOKE -o jsonpath="{range .items[?(@.status.progress.currentStage==\"Done\")]}{.metadata.name}{'\n'}{end}" | wc -l)
+  [ "$AGENTS" == "2" ] && installed=true && break;
+  echo "Waiting for extra worker to be deployed in spoke $SPOKE"
+  sleep 60
+  timeout=$(($timeout + 5))
+done
+if [ "$installed" == "true" ] ; then
+ echo "Extra worker deployed in spoke $SPOKE"
+else
+ echo Timeout waiting for extra worker in $SPOKE to be deployed
 fi
 {% endif %}
 {% endfor %}
