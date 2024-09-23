199ef98807084f2d9c8d21dbf3712085b0b1f89d
karmab <karimboumedhel@gmail.com>
2024-09-23 17:49:47 +0200

    allow to install app cr

--- scripts/10_apps.sh
+++ scripts/10_apps.sh
@@ -8,7 +8,8 @@ export PYTHONUNBUFFERED=true
 {% if app_name == 'users' %}
 kcli create app openshift {{ app_name }} -P users_admin={{ users_admin }} -P users_adminpassword={{ users_adminpassword }} -P users_dev={{ users_dev }} -P users_devpassword={{ users_devpassword }}
 {% else %}
-kcli create app openshift {{ app_name }}{% if app.parameters is defined %}{% for key, value in app.parameters.items() %} -P {{ key }}={{ value }}{% endfor %}{% else %} -P install_cr=false{% endif %}
+{% set install_cr = '-P install_cr=false' if not apps_install_cr else '' %}
+kcli create app openshift {{ app_name }}{% if app.parameters is defined %}{% for key, value in app.parameters.items() %} -P {{ key }}={{ value }}{% endfor %}{% else %} {{ install_cr }}{% endif %}
 
 {% endif %}
 {% endfor %}
