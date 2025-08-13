9b215d49221b6cdc88e46d0476446cf6f6043f6b
karmab <karimboumedhel@gmail.com>
2025-08-13 08:17:38 +0200

    separation and starlink hack

--- ztp/scripts/03_spokes_deploy.sh
+++ ztp/scripts/03_spokes_deploy.sh
@@ -11,18 +11,18 @@ echo $BAREMETAL_IP | grep -q ':' && BAREMETAL_IP=[$BAREMETAL_IP]
 export BAREMETAL_IP
 
 {% for spoke in spokes %}
+SPOKE={{ spoke.name }}
 {% if spoke.get('virtual_nodes', 0) > 0 %}
-kcli delete iso -y {{ spoke.name }}.iso || true
+kcli delete iso -y $SPOKE.iso || true
 {% endif %}
 {% endfor %}
 
-envsubst < /root/ztp/scripts/requirements.sample.yaml > /root/ztp/scripts/requirements.yaml
-envsubst < /root/ztp/scripts/clusterinstances.sample.yaml > /root/ztp/scripts/clusterinstances.yaml
+[ -d /root/spokes ]] || mkdir /root/spokes
+envsubst < /root/ztp/scripts/requirements_$SPOKE.sample.yaml > /spokes/requirements_$SPOKE.yaml
+envsubst < /root/ztp/scripts/clusterinstance_$SPOKE.sample.yaml > /spokes/clusterinstance_$SPOKE.yaml
 
-if [ -f /root/ztp/scripts/snoplus.txt ] ; then
-  for SPOKE in $(cat /root/ztp/scripts/snoplus.txt) ; do
-    sed -i "/$SPOKE-node-1/,$ s/^/##/" /root/ztp/scripts/clusterinstances.yaml
-  done
+if [ -f /root/ztp/scripts/snoplus.txt ] && [ "grep $SPOKE /root/ztp/scripts/snoplus.txt" != "" ] ; then
+  sed -i "/$SPOKE-node-1/,$ s/^/##/" /spoke/clusterinstances.yaml
 fi
 
 bash /root/ztp/scripts/generate_gitops.sh
